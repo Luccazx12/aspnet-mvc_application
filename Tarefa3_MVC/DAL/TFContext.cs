29db5da3fa98cb0d991a1b7f831ba84d0e0e2b54
Lucca <71888383+Luccazx12@users.noreply.github.com>
2021-03-05 14:58:32 -0300

    Add files via upload

---
+++ Tarefa3_MVC/DAL/TFContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Tarefa3_MVC.Models;
+
+namespace Tarefa3_MVC.DAL
+{
+    public class TFContext : DbContext
+    {
+        public TFContext() : base("TFConnectionString") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Universidade> Universidades { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
